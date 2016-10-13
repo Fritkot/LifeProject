c868040146e71bda5ca1741c5d1bca09537b2737
Fritkot <je_te_cule@hotmail.com>
2016-10-13 17:03:20 +0200

    Add files via upload

--- Scripts/Read_Cities_Source_file.py
+++ Scripts/Read_Cities_Source_file.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3.2
+
 #set current working directory
+from sys import path
 from os import chdir
-chdir("C:/Users/Joe/Documents/GitHub/LifeProject/Scripts")
+#chdir("C:/Users/Joe/Documents/GitHub/LifeProject/Scripts")
+#chdir("/home/etu/f/p0400533/LifeProject-master/Scripts")
+chdir(path[0])
 
 #import distances between main european towns
 filePath = '../Inputs/Distances.csv'
 
 townMatrix = dict() #stores all the cities with the distance between the other cities
+townList = list()
 #open Distance file
 with open(filePath,'r') as file:
 
@@ -17,7 +23,9 @@ with open(filePath,'r') as file:
             row = line.split(',') #data are imported from a csv with ',' as a separator
             town = row[0]
             townList.append(town)
-            townMatrix[town] = row[1:]
+	    for i in range(len(townList)) :
+            	townMatrix[town].update((townList[i],row[i]))
+
             #print(line)
             #beware, there are 3 extra columns for Stockholm, Vienna and Warsaw
             #print(line.split(','))
@@ -25,6 +33,8 @@ with open(filePath,'r') as file:
 
 print(townMatrix)
 print(townList)
+print(townMatrix.keys())
+
 #conection to SQLite which is, by default, installed with Pyhton3
 # import sqlite3
 # con = sqlite3.connect(":memory:") #create DB in memory
@@ -34,3 +44,5 @@ print(townList)
 # with open(sql_script_file,'r') as sqlScript:
 #     cur.executescript(sqlScript.read())
 #     sqlScript.close
+#import sys
+#print(sys.path[0])
