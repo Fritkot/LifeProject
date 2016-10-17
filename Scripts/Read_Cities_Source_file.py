0b53605f85b55927003418d822e789e5504fbe9b
Fritkot <je_te_cule@hotmail.com>
2016-10-17 15:12:44 +0200

    Import du graphe depuis csv
    fin de l'import du graphe depuis un fichier csv.

--- Scripts/Read_Cities_Source_file.py
+++ Scripts/Read_Cities_Source_file.py
@@ -1,48 +1,103 @@
 #!/usr/bin/python3.2
 
-#set current working directory
-from sys import path
-from os import chdir
-#chdir("C:/Users/Joe/Documents/GitHub/LifeProject/Scripts")
-#chdir("/home/etu/f/p0400533/LifeProject-master/Scripts")
-chdir(path[0])
+## lecture du fichier source
 
-#import distances between main european towns
-filePath = '../Inputs/Distances.csv'
+def importDonnéesSources(cheminFichier):
+    """
+    prend la matrice des distances en paramètre contenue dans un fichier csv
+    
+    paramètre :
+        cheminFichier : chemin relatif vers le fichier csv, ex : '../Inputs/Distances.csv'
+    return : deux éléments :
+                1) un dictionnaire avec un couple de ville relié en clef avec la distance qui les sépare.
+                2) un liste de toutes les villes considérées
+    
+    exemple :
+        dictionnire : {('Barcelona', 'Belgrade'): 1528.13, ('Belgrade', 'Istanbul'): 809.48}
+        liste       : ['Barcelona', 'Belgrade']
+    """
+    
+    
+    #set current working directory
+    from sys import path
+    from os import chdir
+    import csv
+    
+    chdir(path[0])
+    
+    townMatrix = dict() #stores all the cities with the distance between the other cities
+    townList = list()
+    
+    #open Distance file
+    with open(cheminFichier,'r') as file:
+        
+        #crée un reader
+        reader = csv.reader(file, delimiter=';')
+        
+        #first row is an header row which is not relevant for the moment
+        isHeader = True 
+        for row in reader:
+            if isHeader:
+                isHeader = False #skip the first row
+            else:            
+                town = row[0]
+                townList.append(town)
+                townMatrix[town] = [float(el) for el in row[1:]]
+        file.close()
 
-townMatrix = dict() #stores all the cities with the distance between the other cities
-townList = list()
-#open Distance file
-with open(filePath,'r') as file:
+    # print(townMatrix)
+    # print(townList)
+    # print(townMatrix.keys())
 
-    isHeader = True #first row is an header row which is not relevant for the moment
-    for line in file.readlines():
-        if isHeader:
-            isHeader = False #skip the first row
-        else:
-            row = line.split(',') #data are imported from a csv with ',' as a separator
-            town = row[0]
-            townList.append(town)
-            # for i in range(len(townList)) :
-            #     townMatrix[town].update((townList[i],row[i]))
+    # graphe = dict() #dictionnaire avec un couple de ville comme clef
+    # #construisons le graphe
+    # for ligne in townMatrix.keys():
+    #     for colonne in townMatrix.keys():
+    #         graphe[(ligne,colonne)]=townMatrix[ligne][townList.index(colonne)]
+    # print(graphe[(townList[0],townList[2])])
 
-            #print(line)
-            #beware, there are 3 extra columns for Stockholm, Vienna and Warsaw
-            #print(line.split(','))
-    file.close()
+    graphe = dict() #dictionnaire avec un couple de ville comme clef
+    #construisons le graphe
+    for ligne in townMatrix.keys():
+        for colonne in townMatrix.keys():
+            
+            distance = townMatrix[ligne][townList.index(colonne)]
+            if distance <= 0 :
+                break   # si distance = 0, la ville correspond à elle-même
+                        # si distance < 0, il n'y a pas de liaison entre les deux villes
+            
+            graphe[triePlusPetiteVille(ligne,colonne)]=distance
+    #print(graphe[triePlusPetiteVille(townList[0],townList[2])])
+    return graphe,townList
 
-print(townMatrix)
-print(townList)
-print(townMatrix.keys())
 
-#conection to SQLite which is, by default, installed with Pyhton3
-# import sqlite3
-# con = sqlite3.connect(":memory:") #create DB in memory
-# cur = con.cursor()
-# 
-# sql_script_file = "/media/p0400533/7581-F871/Projet Lif/Inputs/villes_france.sql"
-# with open(sql_script_file,'r') as sqlScript:
-#     cur.executescript(sqlScript.read())
-#     sqlScript.close
-#import sys
-#print(sys.path[0])
+def triePlusPetiteVille(ville1,ville2):
+    """
+    compare de maniere lexicographique le noms des deux villes
+    
+    Paramètres:
+        ville1 : nom de la première ville
+            type : string
+        ville2 : nom de la deuxième ville
+            type : string
+    
+    Return : un tuple avec la ville la plus petite d'un point de vu lexicographique en premier
+        type : tuple de 2 strings
+        
+        
+    exemple :
+        >>> triePlusPetiteVille("Vienna","Brussels")
+        ('Brussels', 'Vienna')
+    """
+    if ville1>ville2 :
+        return (ville2,ville1)
+    else:
+        return (ville1,ville2)
+
+
+## script principal
+
+fichier = '../Inputs/Distances.csv'
+
+graph,villes = importDonnéesSources(fichier)
+
